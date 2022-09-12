676794418b0cee1ba50218ee53639218370392dc
eert1 <1826715363@qq.com>
2022-09-13 00:57:21 +0800

    修改配置文件

--- vue.config.js
+++ vue.config.js
@@ -6,9 +6,7 @@ module.exports = defineConfig({
     host: 'localhost'
   },
   outputDir: 'dist',   //打包的文件名，可以自己设置，默认是dist
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/dist/'
-      : '/'
+  publicPath: './'
 // assetsDir: 'static',
 })
 
